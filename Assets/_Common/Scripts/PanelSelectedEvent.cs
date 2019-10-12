23a2bb1c450bc7ffad14b1768f667b7ba524c36c
onouekento <kentou1506@gmail.com>
2019-10-12 19:08:58 +0900

    ・レベルを選択 →UIをきれいに →選択音追加 ・スタートボタン押す →余裕があればカウントダウン追加 ・レベルの回数分ランダムに光る →できたか怪しい

---
+++ Assets/_Common/Scripts/PanelSelectedEvent.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+/// <summary>
+/// 選ばれたパネルが行うイベントまとめ
+/// パネルにアタッチ (要プレファブ化)
+/// </summary>
+public class PanelSelectedEvent : MonoBehaviour
+{
+    [SerializeField, Header("パネルを光らせる")]
+    Material lightPanel;
+
+    [SerializeField,Header("正解音")]
+    AudioClip correctSE;
+
+    [SerializeField,Header("不正解音")]
+    AudioClip wrongSE;
+
+    MeshRenderer m_thisObjMeshRenderer;
+    Material m_thisObjMaterial;
+
+    AudioSource panelAudio;
+
+    float m_blinkInterval = 1;
+
+    void Start()
+    {
+        //最初に全部取ってきとく
+        m_thisObjMeshRenderer = this.gameObject.GetComponent<MeshRenderer>();
+        m_thisObjMaterial = m_thisObjMeshRenderer.material;
+
+    }
+
+    //正解音
+    public void CorrectSoundPlay()
+    {
+        panelAudio.clip = correctSE;
+        panelAudio.Play();
+    }
+    
+    //不正解音
+    public void WrongSoundPlay()
+    {
+        panelAudio.clip = wrongSE;
+        panelAudio.Play();
+    }
+
+    //パネル点滅
+    public void BlinkPanel()
+    {
+        StartCoroutine(BlinkPanelCoroutine());
+    }
+
+    IEnumerator BlinkPanelCoroutine()
+    {
+        //光が消えるまで操作できないようにするならここにその処理を書く
+
+        m_thisObjMeshRenderer.material = lightPanel;
+        yield return new WaitForSeconds(m_blinkInterval);
+        m_thisObjMeshRenderer.material = m_thisObjMaterial;
+    }
+
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(PanelSelectedEvent))]
+    public class ExplanationWindow : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            {
+                EditorGUILayout.HelpBox("選ばれたパネルが行うイベントまとめ", MessageType.Info);
+            }
+            EditorGUILayout.EndVertical();
+
+            base.OnInspectorGUI();
+
+        }
+    }
+#endif
+}
--- Assets/_Common/Scripts/PanelSelectedRandom.cs
+++ Assets/_Common/Scripts/PanelSelectedRandom.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 /// <summary>
 /// ランダムにパネルが選ばれる
+/// パネルの親にアタッチ
 /// </summary>
 public class PanelSelectedRandom : Singleton<PanelSelectedRandom>
 {
     [SerializeField,Header("スライダーからレベルを取得")]
     LevelGetFromSlider m_levelGetFromSlider;
-
-    [SerializeField, Header("パネルの親から子を全部取得")]
-    GameObject panelParent;
 
     [SerializeField, Header("選ばれる間隔を操作")]
     float m_interval;
@@ -19,37 +20,62 @@ public class PanelSelectedRandom : Singleton<PanelSelectedRandom>
 
     //外部から呼び出してリストを操作
     [HideInInspector]
-    public List<GameObject> listObj = new List<GameObject>();
+    public List<GameObject> g_listObj = new List<GameObject>();
 
     float m_randomValue;
-
-    
+ 
     void Start()
     {
         //子のオブジェクトを全部リストに入れとく
-        foreach(Transform child in panelParent.transform)
+        foreach(Transform child in this.gameObject.transform)
         {
             m_tmpListObj.Add(child.gameObject);
         }
     }
 
-    void Update()
+    //Startボタン押したら呼ばれる
+    public void SelectStart()
     {
-        //ここでどれが選ばれるかランダムに決める
-        m_randomValue = Random.Range(0, m_tmpListObj.Count);
+        StartCoroutine(SelectStartCoroutine());
     }
 
-    //レベルに応じて選ばれる回数を変える
+    //レベルに応じて自動で選ばれる回数を変える
     IEnumerator SelectStartCoroutine()
     {
         for(int i = 0; i < m_levelGetFromSlider.GetLevel(); i++)
         {
             //間隔は後からレベルに応じて変えるかも
             yield return new WaitForSeconds(m_interval);
-            //m_tmpListObj[(int)m_randomValue].GetComponent<パネルについてるコンポーネント>().いろいろ登録したイベント;
+
+            //ここでどれが選ばれるかランダムに決める
+            m_randomValue = Random.Range(0, m_tmpListObj.Count);
+
+            //選ばれたときのイベント呼び出し
+            m_tmpListObj[(int)m_randomValue].GetComponent<PanelSelectedEvent>().BlinkPanel();
+            m_tmpListObj[(int)m_randomValue].GetComponent<PanelSelectedEvent>().CorrectSoundPlay();
 
             //選んだオブジェクトをリスト化する
-            listObj.Add(m_tmpListObj[(int)m_randomValue]); 
+            g_listObj.Add(m_tmpListObj[(int)m_randomValue]); 
+        }
+
+        //自動で選ばれる処理終わったらパネルを選択可能にする
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(PanelSelectedRandom))]
+    public class ExplanationWindow : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            {
+                EditorGUILayout.HelpBox("ランダムにパネルが選ばれる", MessageType.Info);
+            }
+            EditorGUILayout.EndVertical();
+
+            base.OnInspectorGUI();
+
         }
     }
+#endif
 }
